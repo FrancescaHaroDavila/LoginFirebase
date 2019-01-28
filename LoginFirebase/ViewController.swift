27a19176bb9f022ca513ec4d9ff3bcc88d2f256d
Francesca <fharo@belatrixsf.com>
2019-01-28 10:02:06 -0500

    Initial Commit

---
+++ LoginFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginFirebase
+//
+//  Created by Francesca Valeria Haro Dávila on 1/28/19.
+//  Copyright © 2019 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
