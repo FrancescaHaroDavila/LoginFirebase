579e7e2c68aa5d5a2f8c725a6fcfe0e774d4764e
Francesca <fharo@belatrixsf.com>
2019-02-07 09:01:19 -0500

    Login

---
+++ LoginFirebase/ViewControllers/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.swift
+//  LoginFirebase
+//
+//  Created by Francesca Valeria Haro Dávila on 1/28/19.
+//  Copyright © 2019 Belatrix. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class HomeViewController: UIViewController {
+
+  
+  
+  @IBAction func logOutAction(_ sender: UIBarButtonItem) {
+    do {
+    try Auth.auth().signOut()
+  }
+  catch let signOutError as NSError {
+    print ("Error signing out: %@", signOutError)
+    }
+    
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let initial = storyboard.instantiateInitialViewController()
+    UIApplication.shared.keyWindow?.rootViewController = initial
+  }
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
